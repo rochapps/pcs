b88b1c1f454cfb982b164d3dab7aac4ba18bac0a
Victor Rocha <victor@rochapps.com>
2016-02-20 10:08:03 -0500

    [initial] Copies over css and js from app level

--- pcs/primcom/static/primcom/js/add.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(partial(show_new_form, '#id_reference_id', '#id_new_reference'));
-$(document).ready(partial(show_new_form, '#id_taxonomy_id', '#id_new_taxonomy'));
-$(document).ready(partial(show_new_form, '#id_location_id', '#id_new_location'));
-$('#id_reference_id').on('change', partial(show_new_form, '#id_reference_id', '#id_new_reference'))
-$('#id_taxonomy_id').on('change', partial(show_new_form, '#id_taxonomy_id', '#id_new_taxonomy'))
-$('#id_location_id').on('change', partial(show_new_form, '#id_location_id', '#id_new_location'))
--- pcs/static/js/base.js
+++ pcs/static/js/base.js
@@ -8,21 +8,21 @@ function partial(func /*, 0..n args */) {
 
 function show_new_form(trigger_id, form_id) {
     if ($(trigger_id).val() === "-1") {
-        $(form_id).show()
+        $(form_id).show();
     } else {
-        $(form_id).hide()
+        $(form_id).hide();
     }
 }
 
 function select_all(section_id) {
     $(section_id + ' .toggle-button').click( function () {
-        $(section_id + ' input[type="checkbox"]').prop('checked', this.checked)
+        $(section_id + ' input[type="checkbox"]').prop('checked', this.checked);
     })
 }
 
 function select_all_sub(section_id) {
     $(section_id + ' .toggle-button-sub').click( function () {
-        $(section_id + ' input[type="checkbox"]').prop('checked', this.checked)
+        $(section_id + ' input[type="checkbox"]').prop('checked', this.checked);
     })
 }
 
@@ -30,24 +30,24 @@ function select_all_sub(section_id) {
 $( '#species .toggle-button' ).click( function () {
     $( '#species input[type="checkbox"]' ).prop('checked', this.checked)
 })*/
-select_all('#species_raw')
-select_all('#species_ch')
-select_all('#species_wr')
+select_all('#species_raw');
+select_all('#species_ch');
+select_all('#species_wr');
 
 /* Select/Deselect all button for Query Data -> Traits
 $( '#traits .toggle-button' ).click( function () {
     $( '#traits input[type="checkbox"]' ).prop('checked', this.checked)
 })*/
 /*select_all('#traits')*/
-select_all_sub('#category_1')
-select_all_sub('#category_2')
-select_all_sub('#category_3')
-select_all_sub('#category_4')
-select_all_sub('#category_5')
-select_all_sub('#category_6')
-select_all_sub('#category_7')
-select_all_sub('#category_8')
-select_all('#traits')
+select_all_sub('#category_1');
+select_all_sub('#category_2');
+select_all_sub('#category_3');
+select_all_sub('#category_4');
+select_all_sub('#category_5');
+select_all_sub('#category_6');
+select_all_sub('#category_7');
+select_all_sub('#category_8');
+select_all('#traits');
 
 
 /*$("#taxonomy").change(function () {
@@ -67,4 +67,11 @@ $(document).on( "change", "input[name=taxonomy]", function() {
     var tax = $(this).val();
     $(".species_container").hide();
     $("#"+tax).show();
+    $('#id_reference_id').on('change', partial(show_new_form, '#id_reference_id', '#id_new_reference'));
+    $('#id_taxonomy_id').on('change', partial(show_new_form, '#id_taxonomy_id', '#id_new_taxonomy'));
+    $('#id_location_id').on('change', partial(show_new_form, '#id_location_id', '#id_new_location'));
 });
+
+$(document).ready(partial(show_new_form, '#id_reference_id', '#id_new_reference'));
+$(document).ready(partial(show_new_form, '#id_taxonomy_id', '#id_new_taxonomy'));
+$(document).ready(partial(show_new_form, '#id_location_id', '#id_new_location'));
