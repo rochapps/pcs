f4a66437ee79255ea779e17a625ede338750b634
Victor Rocha <victor@rochapps.com>
2016-06-14 21:36:35 -0400

    [deploy] Append project directory to path

--- pcs/wsgi.py
+++ pcs/wsgi.py
@@ -7,11 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 from . import load_env
+
+# Allow Django to find the `app` module by appending the parent directory to
+# the PATH variable
+base = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(base)
 
 load_env.load_env()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pcs.settings")
